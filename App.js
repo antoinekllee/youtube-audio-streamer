dae4f7dc1ce70c994ee005d3c81b03e7a7517bd3
Antoine-Lee <antoinekllee@gmail.com>
2023-05-21 21:21:20 +0800

    playing on ios and fixed slider

--- App.js
+++ App.js
@@ -1,120 +1,144 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Audio } from 'expo-av';
-import Slider from '@react-native-community/slider';
-import { MaterialIcons } from '@expo/vector-icons';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Audio } from "expo-av";
+import Slider from "@react-native-community/slider";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const App = () => {
-  const [audio, setAudio] = useState(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [duration, setDuration] = useState(0);
-  const [position, setPosition] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
+    const [audio, setAudio] = useState(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [duration, setDuration] = useState(0);
+    const [position, setPosition] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
 
-  const youtubeUrl = 'https://www.youtube.com/watch?v=JJNQbAqPwI0';
+    const youtubeUrl = "https://www.youtube.com/watch?v=JJNQbAqPwI0";
 
-  useEffect(() => {
-    (async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch(`http://10.0.1.169:3000/audio-url?videoUrl=${encodeURIComponent(youtubeUrl)}`);
-        const { audioURL } = await response.json();
+    useEffect(() => {
+        (async () => {
+            try {
+                setIsLoading(true);
 
-        console.log('audioURL: ', audioURL);
+                await Audio.setAudioModeAsync({
+                    allowsRecordingIOS: false,
+                    staysActiveInBackground: false,
+                    playsInSilentModeIOS: true,
+                    shouldDuckAndroid: true,
+                    playThroughEarpieceAndroid: false,
+                });
 
-        const { sound } = await Audio.Sound.createAsync({ uri: audioURL });
+                const response = await fetch(
+                    `http://10.0.1.169:3000/audio-url?videoUrl=${encodeURIComponent(
+                        youtubeUrl
+                    )}`
+                );
+                const { audioURL } = await response.json();
 
-        setAudio(sound);
-        setDuration(sound._durationMillis);
+                console.log("audioURL: ", audioURL);
 
-        // Set the listener here, right after creating the audio object
-        sound.setOnPlaybackStatusUpdate((status) => {
-          console.log ("Updating position to " + status.positionMillis + "/" + duration)
-          setPosition(status.positionMillis);
-          if (status.didJustFinish) {
-            console.log('COMPLETE');
-            setIsPlaying(false);
-          }
-        });
+                const { sound } = await Audio.Sound.createAsync({
+                    uri: audioURL,
+                });
 
-        setIsLoading(false);
+                setAudio(sound);
 
-      } catch (error) {
-        console.error('Error fetching audio URL:', error);
-        setIsLoading(false);
-      }
-    })();
+                sound.setOnPlaybackStatusUpdate((status) => {
+                  console.log(
+                      "Updating position to " +
+                          status.positionMillis +
+                          "/" +
+                          status.durationMillis
+                  );
+                  setPosition(status.positionMillis);
+                  setDuration(status.durationMillis);
+                  if (status.didJustFinish) {
+                      console.log("COMPLETE");
+                      setIsPlaying(false);
+                  }
+                });
 
-    return () => {
-      if (audio) {
-        audio.unloadAsync();
-      }
+                setIsLoading(false);
+            } catch (error) {
+                console.error("Error fetching audio URL:", error);
+                setIsLoading(false);
+            }
+        })();
+
+        return () => {
+            if (audio) {
+                audio.unloadAsync();
+            }
+        };
+    }, []);
+
+    const handlePlayPause = async () => {
+        if (isPlaying) {
+            await audio.pauseAsync();
+        } else {
+            const playbackStatus = await audio.playAsync();
+            setPosition(playbackStatus.positionMillis);
+        }
+        setIsPlaying(!isPlaying);
     };
-  }, []);
 
-  const handlePlayPause = async () => {
-    if (isPlaying) {
-      await audio.pauseAsync();
-    } else {
-      const playbackStatus = await audio.playAsync();
-      setPosition(playbackStatus.positionMillis);
-    }
-    setIsPlaying(!isPlaying);
-  };
+    const handleSliderValueChange = async (value) => {
+        if (audio) {
+            await audio.setPositionAsync(value);
+        }
+    };
 
-  const handleSliderValueChange = async (value) => {
-    if (audio) {
-      await audio.setPositionAsync(value);
-    }
-  };
+    return (
+        <View style={styles.container}>
+            {isLoading ? (
+                <Text style={styles.loading}>Loading audio...</Text>
+            ) : (
+                <>
+                    <TouchableOpacity
+                        onPress={handlePlayPause}
+                        disabled={!audio}
+                    >
+                        <MaterialIcons
+                            name={
+                                isPlaying
+                                    ? "pause-circle-outline"
+                                    : "play-circle-outline"
+                            }
+                            size={48}
+                            color={"#3d5875"}
+                        />
+                    </TouchableOpacity>
 
-  return (
-    <View style={styles.container}>
-      {isLoading ? (
-        <Text style={styles.loading}>Loading audio...</Text>
-      ) : (
-        <>
-          <TouchableOpacity onPress={handlePlayPause} disabled={!audio}>
-            <MaterialIcons
-              name={isPlaying ? 'pause-circle-outline' : 'play-circle-outline'}
-              size={48}
-              color={'#3d5875'}
-            />
-          </TouchableOpacity>
-
-          <Slider
-            style={styles.slider}
-            minimumValue={0}
-            maximumValue={duration}
-            value={position}
-            onValueChange={handleSliderValueChange}
-            onSlidingComplete={handleSliderValueChange}
-            minimumTrackTintColor="#3d5875"
-            maximumTrackTintColor="#d3d3d3"
-            thumbTintColor="#3d5875"
-          />
-        </>
-      )}
-    </View>
-  );
+                    <Slider
+                      style={styles.slider}
+                      minimumValue={0}
+                      maximumValue={duration}
+                      value={position}
+                      onSlidingComplete={handleSliderValueChange}
+                      minimumTrackTintColor="#3d5875"
+                      maximumTrackTintColor="#d3d3d3"
+                      thumbTintColor="#3d5875"
+                  />
+                </>
+            )}
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f7f7f7',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  slider: {
-    width: '80%',
-    marginTop: 20,
-  },
-  loading: {
-    fontSize: 18,
-    marginBottom: 20,
-    color: '#3d5875',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#f7f7f7",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    slider: {
+        width: "80%",
+        marginTop: 20,
+    },
+    loading: {
+        fontSize: 18,
+        marginBottom: 20,
+        color: "#3d5875",
+    },
 });
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,53 +1,71 @@
-const express = require('express');
-const ytdl = require('ytdl-core');
-const cors = require('cors');
-const ffmpeg = require('fluent-ffmpeg');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const ytdl = require("ytdl-core");
+const cors = require("cors");
+const ffmpeg = require("fluent-ffmpeg");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.timeout = 120000; // Set the timeout to 2 minutes
 
-app.use('/temp', express.static(path.join(__dirname, 'temp')));
+app.use("/temp", express.static(path.join(__dirname, "temp")));
 
-app.get('/audio-url', async (req, res) => {
+app.get("/audio-url", async (req, res) => {
     const videoUrl = req.query.videoUrl;
     try {
-      const info = await ytdl.getInfo(videoUrl);
-      const audioURL = ytdl.chooseFormat(info.formats, { filter: 'audioonly' }).url;
-  
-      // Define the output file path
-      const outputFilePath = path.join(__dirname, 'temp', `${Date.now()}.mp3`);
-  
-      // Create a writable stream to save the converted audio
-      const writeStream = fs.createWriteStream(outputFilePath);
-  
-      // Use FFmpeg to convert the audio format
-      ffmpeg()
-        .input(audioURL)
-        .format('mp3')
-        .audioCodec('libmp3lame')
-        .pipe(writeStream);
-  
-      // When the conversion is done, send the URL of the converted file
-      writeStream.on('finish', () => {
-        res.json({ audioURL: `${req.protocol}://${req.get('host')}/temp/${path.basename(outputFilePath)}` });
-      });
-  
-      // Handle errors during the conversion
-      writeStream.on('error', (error) => {
-        console.error('Error during audio conversion:', error);
-        res.status(500).json({ error: 'Error during audio conversion' });
-      });
-  
+        const info = await ytdl.getInfo(videoUrl);
+        const audioURL = ytdl.chooseFormat(info.formats, {
+            filter: "audioonly",
+        }).url;
+
+        const outputFilePath = path.join(
+            __dirname,
+            "temp",
+            `${Date.now()}.mp3`
+        ); // Define the output file path
+
+        const writeStream = fs.createWriteStream(outputFilePath); // Create a writable stream to save the converted audio
+
+        // Use FFmpeg to convert the audio format
+        ffmpeg()
+            .input(audioURL)
+            .inputOptions(["-rw_timeout 10000000", "-timeout 6000000"]) // Timeout set to 10 seconds for network operations
+            .format("mp3")
+            .audioCodec("libmp3lame")
+            .on("end", function () {
+                console.log("Conversion finished!");
+                if (!res.headersSent) {
+                    res.json({
+                        audioURL: `${req.protocol}://${req.get(
+                            "host"
+                        )}/temp/${path.basename(outputFilePath)}`,
+                    });
+                }
+            })
+            .on("error", function (err, stdout, stderr) {
+                console.error("Error:", err);
+                console.error("ffmpeg stdout:", stdout);
+                console.error("ffmpeg stderr:", stderr);
+                if (!res.headersSent) {
+                    res.status(500).json({
+                        error: "Error during audio conversion",
+                    });
+                }
+            })
+            .on("start", function (commandLine) {
+                console.log("Spawned Ffmpeg with command: " + commandLine);
+            })
+            .pipe(writeStream, { end: true }); // Pipe the output of ffmpeg to the file
     } catch (error) {
-      console.error('Error fetching audio URL:', error);
-      res.status(500).json({ error: 'Error fetching audio URL' });
+        console.error("Error fetching audio URL:", error);
+        if (!res.headersSent) {
+            res.status(500).json({ error: "Error fetching audio URL" });
+        }
     }
-  });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
